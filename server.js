7ef1e4105a16f5b9c20b03dd20ab89b2f2021103
Erik <erik02003@mail.ru>
2022-05-23 00:32:48 +0600

    dsf

--- server.js
+++ server.js
@@ -712,11 +712,6 @@ app.post("/buy", function(req, res) {
   })
 })
 
-let port = process.env.PORT;
-if (port == null || port = "") {
-  port = 3000;
-}
-
-app.listen(port, function() {
-  console.log("Server has started");
-})
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
